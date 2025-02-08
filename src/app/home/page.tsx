1e55dfe5031cccb5a44eb87d9722df22dacf5f75
syedabaqiri <AimanBaqiri@outlook.com>
2025-02-09 01:15:26 +0500

    trying to resolve vercel error

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 import Navigation from '../../components/Navigation';
 
-import styles from '../styles/Home.module.css';
+import styles from '../../styles/Home.module.css';
 
 export default function Home() {
   return (
