3cef6ff241b9e06dc42b16edd6b86a067e73fa43
syedabaqiri <AimanBaqiri@outlook.com>
2025-02-09 01:10:07 +0500

    error resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react";
-import styles from '../styles/Home.module.css';
+import styles from '../../styles/Home.module.css';
 
 
 export default function Home() {
