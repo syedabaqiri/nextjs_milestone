73f75175c501ba8fe5656752710487e51926a982
syedabaqiri <AimanBaqiri@outlook.com>
2025-02-09 01:03:32 +0500

    commit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,5 @@
 import Navigation from '../../components/Navigation';
+
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <Navigation />
       <h1 className={styles.heading}>Welcome to My Portfolio</h1>
-      {/* Add more content */}
+      
     </div>
   );
 }
